d06de3b7a1009b8e22fc0dda67bcfd5a2a5c5102
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-10-20 10:13:03 +0200

    Added messages to unimplemented menu items. Fixed some formatting.

--- TestApplication/FormAbout.cs
+++ TestApplication/FormAbout.cs
@@ -10,27 +10,27 @@ using System.Windows.Forms;
 
 namespace TestApplication
 {
-  public partial class FormAbout : Form
-  {
-    public FormAbout()
-    {
-      InitializeComponent();
-    }
+	public partial class FormAbout : Form
+	{
+		public FormAbout()
+		{
+			InitializeComponent();
+		}
 
-    private void linkYoutube_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-    {
-      Process.Start("https://www.youtube.com/user/Switcherlapp97");
-    }
+		private void linkYoutube_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			Process.Start("https://www.youtube.com/user/Switcherlapp97");
+		}
 
-    private void linkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-    {
-      Process.Start("https://github.com/Switcherlapp97/RubiksCubeSolver");
-    }
+		private void linkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			Process.Start("https://github.com/Switcherlapp97/RubiksCubeSolver");
+		}
 
-    private void linkMail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-    {
-      Process.Start("mailto:switcherlapp.97@gmail.com");
-    }
+		private void linkMail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			Process.Start("mailto:switcherlapp.97@gmail.com");
+		}
 
 		private void btnClose_Click(object sender, EventArgs e)
 		{
--- TestApplication/FormMain.cs
+++ TestApplication/FormMain.cs
@@ -17,144 +17,149 @@ using FridrichSolver;
 
 namespace TestApplication
 {
-  public partial class FormMain : Form
-  {
-    PluginCollection<CubeSolver> solverPlugins = new PluginCollection<CubeSolver>();
-    BindingList<IMove> rotations = new BindingList<IMove>();
+	public partial class FormMain : Form
+	{
+		PluginCollection<CubeSolver> solverPlugins = new PluginCollection<CubeSolver>();
+		BindingList<IMove> rotations = new BindingList<IMove>();
 
-    public FormMain()
-    {
-      InitializeComponent();
+		public FormMain()
+		{
+			InitializeComponent();
 
-      //foreach (string path in Properties.Settings.Default.PluginPaths)
-      //{
-      //  solverPlugins.AddDll(path);
-      //}
+			//foreach (string path in Properties.Settings.Default.PluginPaths)
+			//{
+			//  solverPlugins.AddDll(path);
+			//}
 
-      cubeModel.StartRender();
+			cubeModel.StartRender();
 
-      foreach (CubeFlag flag in Enum.GetValues(typeof(CubeFlag)))
-      {
-        if (flag != CubeFlag.None && flag != CubeFlag.XFlags && flag != CubeFlag.YFlags && flag != CubeFlag.ZFlags)
-          comboBoxLayers.Items.Add(flag.ToString());
-      }
+			foreach (CubeFlag flag in Enum.GetValues(typeof(CubeFlag)))
+			{
+				if (flag != CubeFlag.None && flag != CubeFlag.XFlags && flag != CubeFlag.YFlags && flag != CubeFlag.ZFlags)
+					comboBoxLayers.Items.Add(flag.ToString());
+			}
 
-      listBoxQueue.DataSource = rotations;
-      listBoxQueue.DisplayMember = "Name";
-    }
+			listBoxQueue.DataSource = rotations;
+			listBoxQueue.DisplayMember = "Name";
+		}
 
 
-    private void loadToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      FolderBrowserDialog fbd = new FolderBrowserDialog();
-      if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-      {
-        solverPlugins.AddFolder(fbd.SelectedPath);
-      }
-    }
+		private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				solverPlugins.AddFolder(fbd.SelectedPath);
+			}
+		}
 
-    private void resetToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      cubeModel.ResetCube();
-    }
+		private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			cubeModel.ResetCube();
+		}
 
-    private void scrambleToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      cubeModel.Rubik.Scramble(50);
-    }
+		private void scrambleToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			cubeModel.Rubik.Scramble(50);
+		}
 
-    private void solveToolStripMenuItem1_Click(object sender, EventArgs e)
-    {
-      DialogSolutionFinder dlg = new DialogSolutionFinder(new TwoPhaseAlgorithm(), this.cubeModel.Rubik, this);
-      if (dlg.ShowDialog() == DialogResult.OK)
-      {
-        rotations.Clear();
-        dlg.Algorithm.Moves.ForEach(m => rotations.Add(m));
-      }
-    }
+		private void solveToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			DialogSolutionFinder dlg = new DialogSolutionFinder(new TwoPhaseAlgorithm(), this.cubeModel.Rubik, this);
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				rotations.Clear();
+				dlg.Algorithm.Moves.ForEach(m => rotations.Add(m));
+			}
+		}
 
-    private void parityTestToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      DialogParityCheckResult parityCheck = new DialogParityCheckResult(cubeModel.Rubik, this);
-      parityCheck.ShowDialog();
-    }
+		private void parityTestToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			DialogParityCheckResult parityCheck = new DialogParityCheckResult(cubeModel.Rubik, this);
+			parityCheck.ShowDialog();
+		}
 
-    private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      using (SaveFileDialog sfd = new SaveFileDialog())
-      {
-        sfd.Filter = "XML-Files|*.xml";
-        if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-        {
-          cubeModel.SavePattern(sfd.FileName);
-        }
-      }
-    }
+		private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			using (SaveFileDialog sfd = new SaveFileDialog())
+			{
+				sfd.Filter = "XML-Files|*.xml";
+				if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					cubeModel.SavePattern(sfd.FileName);
+				}
+			}
+		}
 
-    private void openToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      using (OpenFileDialog ofd = new OpenFileDialog())
-      {
-        ofd.Filter = "XML-Files|*.xml";
-        if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-        {
-          cubeModel.LoadPattern(ofd.FileName);
-        }
-      }
-    }
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog ofd = new OpenFileDialog())
+			{
+				ofd.Filter = "XML-Files|*.xml";
+				if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					cubeModel.LoadPattern(ofd.FileName);
+				}
+			}
+		}
 
-    private void btnRotate_Click(object sender, EventArgs e)
-    {
-      CubeFlag layer = (CubeFlag)Enum.Parse(typeof(CubeFlag), comboBoxLayers.SelectedItem.ToString());
-      cubeModel.RotateLayerAnimated(layer, checkBoxDirection.Checked);
-    }
+		private void btnRotate_Click(object sender, EventArgs e)
+		{
+			CubeFlag layer = (CubeFlag)Enum.Parse(typeof(CubeFlag), comboBoxLayers.SelectedItem.ToString());
+			cubeModel.RotateLayerAnimated(layer, checkBoxDirection.Checked);
+		}
 
-    private void btnClear_Click(object sender, EventArgs e)
-    {
-      this.rotations.Clear();
-    }
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			this.rotations.Clear();
+		}
 
-    private void btnAddToQueue_Click(object sender, EventArgs e)
-    {
-      CubeFlag layer = (CubeFlag)Enum.Parse(typeof(CubeFlag), comboBoxLayers.SelectedItem.ToString());
-      this.rotations.Add(new LayerMove(layer, checkBoxDirection.Checked));
-    }
+		private void btnAddToQueue_Click(object sender, EventArgs e)
+		{
+			CubeFlag layer = (CubeFlag)Enum.Parse(typeof(CubeFlag), comboBoxLayers.SelectedItem.ToString());
+			this.rotations.Add(new LayerMove(layer, checkBoxDirection.Checked));
+		}
 
-    private void btnExecute_Click(object sender, EventArgs e)
-    {
-      foreach (IMove move in rotations)
-        cubeModel.RotateLayerAnimated(move);
-    }
+		private void btnExecute_Click(object sender, EventArgs e)
+		{
+			foreach (IMove move in rotations)
+				cubeModel.RotateLayerAnimated(move);
+		}
 
-    private void manageSolversToolStripMenuItem_Click(object sender, EventArgs e)
-    {
+		private void manageSolversToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			NotImplemented();
+		}
 
-    }
+		private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			NotImplemented();
+		}
 
-    private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
-    {
+		private void NotImplemented()
+		{
+			MessageBox.Show("Not implemented yet.");
+		}
 
-    }
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
 
-    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      Application.Exit();
-    }
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			FormAbout frmAbout = new FormAbout();
+			frmAbout.ShowDialog();
+		}
 
-    private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      FormAbout frmAbout = new FormAbout();
-      frmAbout.ShowDialog();
-    }
+		private void FormMain_Activated(object sender, EventArgs e)
+		{
+			//cubeModel.StartRender();
+		}
 
-    private void FormMain_Activated(object sender, EventArgs e)
-    {
-      //cubeModel.StartRender();
-    }
-
-    private void FormMain_Deactivate(object sender, EventArgs e)
-    {
-      //cubeModel.StopRender();
-    }
-  }
+		private void FormMain_Deactivate(object sender, EventArgs e)
+		{
+			//cubeModel.StopRender();
+		}
+	}
 }
